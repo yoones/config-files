c69073a6bbc8ae7a1a1b1eb1f5a9db814a68de1c
Younes SERRAJ <younes.serraj@gmail.com>
2016-09-14 17:43:05 +0200

    Added alias gogems to find ruby gems' directory

---
+++ my/bin/find_rails_gems.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def cd(path = "")
+  ret = false
+  if path == ""
+    ret = Dir.chdir
+  else
+    ret = Dir.chdir(path)
+  end
+  exit 1 if ret == false
+end
+
+cd
+cd ".rbenv/versions"
+version = `ls -dt * | head -n 1 | tr -d '\n'`
+cd version
+cd "lib/ruby/gems"
+version = `ls -dt * | head -n 1 | tr -d '\n'`
+cd version
+
+puts Dir.pwd
+
+exit 0
