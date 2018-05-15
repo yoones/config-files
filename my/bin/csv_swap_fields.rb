7220695622170029014e4f5e04caaaa7de71d637
Younes SERRAJ <younes.serraj@gmail.com>
2018-05-15 17:09:08 +0200

    Bugfix, added delimiter option (-d)

--- my/bin/csv_swap_fields.rb
+++ my/bin/csv_swap_fields.rb
@@ -1,23 +1,25 @@
 #!/usr/bin/env ruby
 
+sep = ';'
+if ARGV.first == "-d" && ARGV[1].length == 1
+  sep = ARGV[1]
+  2.times { ARGV.shift }
+end
+
 if ARGV.count != 1
-  puts ARGV.count.to_s
   $stderr.puts <<EOF
-Usage: csv_swap_fields.rb "1,4,3,2"
-First field's index is 1 (not 0).
+Usage: csv_swap_fields.rb [-d 'c'] <order>
+  -d    : Delimiter (a single char). Default value: ";"
+  order : First field's index is 1 (not 0). Example: "1,4,3,2"
 EOF
   exit 1
 end
 
-fields = ARGV.first.split(",")
+fields = ARGV.first.split(sep).map { |i| i.to_i }
 
 while line = $stdin.gets
-  f = line.gsub("\n", "").split(';')
-  out = []
-  fields.each do |idx|
-    out << f[idx.to_i - 1]
-  end
-  puts out.join(";")
+  f = line.chomp.split(sep)
+  puts fields.collect { |idx| f[idx - 1] }.join(sep)
 end
 
 exit 0
